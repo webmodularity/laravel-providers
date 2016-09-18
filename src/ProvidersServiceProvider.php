59369e164fc16f42d69642a898b290f2d64dd065
Rory Hellyer <rory@webmodularity.com>
2016-09-17 17:18:44 -0700

    initial commit

---
+++ src/ProvidersServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace WebModularity\LaravelProviders;
+
+use Illuminate\Support\ServiceProvider;
+
+class ProvidersServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+
+    }
+
+    public function boot() {
+
+    }
+}
